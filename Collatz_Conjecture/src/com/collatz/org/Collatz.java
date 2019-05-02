ca94063110e6724272ca5027a6d03af9cdebc340
shubham.shivhare <shubham.shivhare@INL-H09YPN2.groupinfra.com>
2019-05-02 08:22:28 -0400

    initial commit

---
+++ Collatz_Conjecture/src/com/collatz/org/Collatz.java
@@ -0,0 +1,46 @@
+package com.collatz.org;
+
+import java.util.Scanner;
+
+public class Collatz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number");
+		int x = sc.nextInt();
+		Conjecture c = new Conjecture();
+
+		while (x != 1) {
+			if (c.isEven(x)) {
+				x = x / 2;
+				System.out.println(x);
+			}
+
+			else if (c.isODD(x)) {
+				x = (x * 3) + 1;
+				System.out.println(x);
+			}
+
+		}
+	}
+
+}
+
+class Conjecture {
+	boolean isEven(int num) {
+		if (num % 2 == 0) {
+			return true;
+		} else
+			return false;
+	}
+
+	boolean isODD(int num) {
+		if (num % 2 != 0) {
+			return true;
+		} else
+			return false;
+	}
+}
